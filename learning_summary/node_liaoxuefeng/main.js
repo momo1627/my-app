ceb50a426bf0eead0cb9081133997fddb1d1b7d1
momo1627 <momo1627@gmail.com>
2018-12-16 11:13:43 +1100

    promise

--- learning_summary/node_liaoxuefeng/main.js
+++ learning_summary/node_liaoxuefeng/main.js
@@ -1,2 +1,23 @@
-import greet from './01_node'
-greet('daniel')
+var {greet,bye} = require('./01_node')
+global.process.on('exit',
+    ()=>{
+     bye('daniel')
+    }
+);
+greet('daniel');
+var fs = require('fs');
+var rs = fs.createReadStream('sample.txt','utf-8');
+rs.on('data',function(chunk){
+    console.log('DATA:');
+    console.log(chunk)
+})
+rs.on('end',function(){
+    console.log('END');
+})
+rs.on('error',(error)=>{
+    console.log(`ERROR: ${error}`)
+})
+
+var ws = fs.createWriteStream('ouput.txt','utf-8');
+
+rs.pipe(ws);

7bfd7f774522b443b22666fe6e9f044a2769815e
momo1627 <momo1627@gmail.com>
2018-12-19 16:24:40 +1100

    generator

--- react-practice/src/GoogleMap/GoogleMap.js
+++ react-practice/src/GoogleMap/GoogleMap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 /*global google*/
 import axios from 'axios'
-const API_KEY = 'AIzaSyC0W_7Xof88qi51CnXgWEJVSOxyJFeKzME'
+
 class GoogleMap extends React.Component {
   constructor(props) {
     super(props);

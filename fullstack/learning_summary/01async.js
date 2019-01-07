4a038e94c2bad31a32a70ceb1eae5ea3d1bdf69d
momo1627 <momo1627@gmail.com>
2019-01-08 06:10:01 +1100

    01async

---
+++ fullstack/learning_summary/01async.js
@@ -0,0 +1,12 @@
+// project1中 async使用 
+// 1.async函数返回一个promise 函数内return的内容，就是promise对象 resolve的参数
+// 2.await后面的promise对象，知道promise resolve才继续函数内容，reject则函数中止
+// 3.await promise对象的结果，会得到promise对象 resolve的参数。这是基于generator的执行器，会将promise的结果，再替代给yield 语句
+// 4.总而言之，promise和async 重要的是resolve状态的参数。在axios或fetch请求中，即是 response
+var fetch = require('fetch')
+const url = 'http://localhost:8000/items';
+async function request(url){
+    const response = await fetch(url,{method:'GET'});
+    return response;
+}
+request(url).then(x=>{console.log(x)})
